9cd16c3608fb64479a9c6e548f96e6b9ca8d63b8
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2022-01-14 00:59:52 +0500

    sounds cashed

--- music_player.py
+++ music_player.py
@@ -4,15 +4,22 @@ import pygame as pg
 class MusicPlayer:
     def __init__(self):
         pg.mixer.init()
+        self.sound_dict = {}
 
     def play_sound(self, file):
-        sound = pg.mixer.Sound(file)
+        if file in self.sound_dict:
+            sound = self.sound_dict[file]
+        else:
+            sound = pg.mixer.Sound(file)
+
         sound.play()
 
-    def play_bg_music(self):
+    @staticmethod
+    def play_bg_music():
         pg.mixer.music.load('./data/sounds/bg_music.mp3')
         pg.mixer.music.set_volume(0.05)
         pg.mixer.music.play(-1)
 
-    def pause_bg_music(self):
+    @staticmethod
+    def pause_bg_music():
         pg.mixer.music.stop()
